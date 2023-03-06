1f445ecd1d64f533a1c3110f5c8325a55ba0e28b
raisircl <rai.verma@live.com>
2023-03-06 13:53:31 +0530

    list box and combo

---
+++ WinFormsAppManish/FrmList.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WinFormsAppManish
+{
+    partial class FrmList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCourses = new System.Windows.Forms.ListBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblCourses = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbCourses
+            // 
+            this.lbCourses.FormattingEnabled = true;
+            this.lbCourses.ItemHeight = 20;
+            this.lbCourses.Items.AddRange(new object[] {
+            "C",
+            "C++"});
+            this.lbCourses.Location = new System.Drawing.Point(23, 12);
+            this.lbCourses.Name = "lbCourses";
+            this.lbCourses.Size = new System.Drawing.Size(158, 184);
+            this.lbCourses.TabIndex = 0;
+            this.lbCourses.SelectedIndexChanged += new System.EventHandler(this.lbCourses_SelectedIndexChanged);
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(23, 213);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(161, 27);
+            this.txtCourse.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(23, 246);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblCourses
+            // 
+            this.lblCourses.AutoSize = true;
+            this.lblCourses.Location = new System.Drawing.Point(304, 21);
+            this.lblCourses.Name = "lblCourses";
+            this.lblCourses.Size = new System.Drawing.Size(50, 20);
+            this.lblCourses.TabIndex = 3;
+            this.lblCourses.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Display All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "C",
+            "C++",
+            "VB",
+            "Java"});
+            this.comboBox1.Location = new System.Drawing.Point(409, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FrmList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCourses);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.lbCourses);
+            this.Name = "FrmList";
+            this.Text = "FrmList";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox lbCourses;
+        private TextBox txtCourse;
+        private Button btnAdd;
+        private Label lblCourses;
+        private Button button1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ WinFormsAppManish/FrmList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppManish
+{
+    public partial class FrmList : Form
+    {
+        public FrmList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!lbCourses.Items.Contains(txtCourse.Text))
+                lbCourses.Items.Add(txtCourse.Text);
+            else
+                MessageBox.Show("Item allready exist");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = lbCourses.Items.Count;
+            lblCourses.Text = "";
+            for (int i = 0; i < x; i++)
+            {
+                lblCourses.Text += lbCourses.Items[i].ToString() + "\n";
+            }
+        }
+
+        private void lbCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lbCourses.SelectedIndex>-1)
+            {
+                lblCourses.Text = lbCourses.SelectedItem.ToString();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                lblCourses.Text = comboBox1.SelectedItem.ToString();
+            }
+        }
+    }
+}
